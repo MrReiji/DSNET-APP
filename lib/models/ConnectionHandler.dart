7d5ddd1b03a978819064a1ea7a7c922a6811a385
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2023-03-29 00:53:43 +0200

    Working with post request

--- lib/models/ConnectionHandler.dart
+++ lib/models/ConnectionHandler.dart
@@ -1,34 +1,59 @@
-import 'dart:html';
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' as parser;
 import 'dart:convert';
+import 'dart:io';
+import 'package:dio_logging_interceptor/dio_logging_interceptor.dart';
+import 'package:flutter/material.dart';
+import 'package:html/parser.dart' as parser;
+import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:cookie_jar/cookie_jar.dart';
+
+Dio dio = Dio();
+CookieJar cookieJar = CookieJar();
 
 class ConnectionHandler {
   //final String urlStr = "https://panel.dsnet.agh.edu.pl/reserv/rezerwuj/2889";
   static Future<void> getData() async {
-    final url =
-        Uri.parse("https://panel.dsnet.agh.edu.pl/reserv/rezerwuj/2889");
+    const url = "https://panel.dsnet.agh.edu.pl/reserv/rezerwuj/2889";
     try {
-      final response = await http.get(url);
-      print(response.body);
+      final response = await dio.get(
+        url,
+      );
+      print(response.data.toString());
     } catch (error) {
       throw (error);
     }
   }
 
   static Future<bool> postData(Map<String, String> data) async {
-    final url = Uri.parse("https://panel.dsnet.agh.edu.pl/login_check");
+    const url = "https://panel.dsnet.agh.edu.pl/login_check";
     try {
-      final response = await http.post(url, body: data);
-      print(response.body);
+      final response = await dio.post(url,
+          data: data,
+          options: Options(headers: {
+            'accept':
+                'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cache-control': 'max-age=0',
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'https://panel.dsnet.agh.edu.pl',
+            'referer': 'https://panel.dsnet.agh.edu.pl/login',
+          }));
+
+      dio.interceptors.add(CookieManager(cookieJar));
+      final cookies = response.headers['set-cookie'];
+      print(cookies);
+      dio.interceptors.add(DioLoggingInterceptor(
+        level: Level.body,
+        compact: false,
+      ));
+      //print(response.data);
       var elem = parser
-          .parse(response.body)
-          .getElementsByClassName("tour_welcome")
+          .parse(response.data)
+          .getElementsByClassName('tour_welcome')
           .isEmpty;
-      print(elem);
-      print(data);
+      //print(elem);
+      //print(data);
       return response.statusCode == 200;
     } catch (error) {
       throw (error);
